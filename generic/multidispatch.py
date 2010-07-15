665977399e76dbfb3f998284dbfc0f23abbd2b6d
Andrey Popp <8mayday@gmail.com>
2010-07-15 17:05:54 +0400

    Allow partitional (by arguments) dispatching.

--- generic/multidispatch.py
+++ generic/multidispatch.py
@@ -16,10 +16,12 @@ def multimethod(*arg_types):
     """ Declare function as multimethod."""
     global dispatchers
     def register_rule(func):
+        argspec = inspect.getargspec(func)
         if func.__name__ in dispatchers:
             dispatcher = dispatchers[func.__name__]
         else:
-            dispatcher = functools.wraps(func)(Dispatcher(len(arg_types)))
+            dispatcher = functools.wraps(func)(
+                Dispatcher(argspec, len(arg_types)))
             dispatchers[func.__name__] = dispatcher
         dispatcher.register_rule(func, *arg_types)
         return dispatcher
@@ -35,24 +37,31 @@ def reset():
 class Dispatcher(object):
     """ Function call dispatcher based on argument types."""
 
-    def __init__(self, arity):
-        self.arity = arity
-        axis = [("arg_%d" % n, TypeAxis()) for n in range(arity)]
+    def __init__(self, argspec, multi_arity):
+        pos_arity = \
+            len(argspec.args if argspec.args else []) - \
+            len(argspec.defaults if argspec.defaults else [])
+        if pos_arity < multi_arity:
+            raise TypeError("Not enough positional arguments "
+                            "for number of type parameters provided.")
+        self.argspec = argspec
+        self.multi_arity = multi_arity
+        axis = [("arg_%d" % n, TypeAxis()) for n in range(multi_arity)]
         self.registry = Registry(*axis)
 
     def register_rule(self, rule, *args):
+        if len(args) != self.multi_arity:
+            raise TypeError("Wrong number of type parameters.")
         self.check_rule(rule)
         self.registry.register(rule, *args)
 
     def lookup_rule(self, *args):
-        return self.registry.lookup(*args)
+        return self.registry.lookup(*args[:self.multi_arity])
 
     def check_rule(self, rule):
         argspec = inspect.getargspec(rule)
-        if argspec.defaults:
-            raise NotImplementedError("Keyword argument support "
-                                      "not implemented yet.")
-        if not len(argspec.args) == self.arity:
+        if not map(lambda x: len(x) if x else 0, argspec) == \
+          map(lambda x: len(x) if x else 0, self.argspec):
             raise TypeError("Rule does not conform "
                             "to previous implementations.")
 
--- generic/tests/test_multidispatch.py
+++ generic/tests/test_multidispatch.py
@@ -2,49 +2,54 @@
 
 import unittest
 
-__all__ = []
+__all__ = ["DispatcherTests"]
 
 
 class DispatcherTests(unittest.TestCase):
 
-    def test_one_argument(self):
+    def createDispatcher(self, multi_arity, args=None, varargs=None,
+                         keywords=None, defaults=None):
+        from inspect import ArgSpec
         from generic.multidispatch import Dispatcher
-        dispatcher = Dispatcher(1)
+        return Dispatcher(ArgSpec(args=args, varargs=varargs, keywords=keywords,
+                                  defaults=defaults), multi_arity)
+
+
+    def test_one_argument(self):
+        dispatcher = self.createDispatcher(1, args=["x"])
 
         dispatcher.register_rule(lambda x: x + 1, int)
         self.assertEqual(dispatcher(1), 2)
-        self.assertRaises(TypeError, dispatcher.__call__, "s")
+        self.assertRaises(TypeError, dispatcher, "s")
 
         dispatcher.register_rule(lambda x: x + "1", str)
         self.assertEqual(dispatcher(1), 2)
         self.assertEqual(dispatcher("1"), "11")
-        self.assertRaises(TypeError, dispatcher.__call__, tuple())
+        self.assertRaises(TypeError, dispatcher, tuple())
 
     def test_two_arguments(self):
-        from generic.multidispatch import Dispatcher
-        dispatcher = Dispatcher(2)
+        dispatcher = self.createDispatcher(2, args=["x", "y"])
 
         dispatcher.register_rule(lambda x, y: x + y + 1, int, int)
         self.assertEqual(dispatcher(1, 2), 4)
-        self.assertRaises(TypeError, dispatcher.__call__, "s", "ss")
-        self.assertRaises(TypeError, dispatcher.__call__, 1, "ss")
-        self.assertRaises(TypeError, dispatcher.__call__, "s", 2)
+        self.assertRaises(TypeError, dispatcher, "s", "ss")
+        self.assertRaises(TypeError, dispatcher, 1, "ss")
+        self.assertRaises(TypeError, dispatcher, "s", 2)
 
         dispatcher.register_rule(lambda x, y: x + y + "1", str, str)
         self.assertEqual(dispatcher(1, 2), 4)
         self.assertEqual(dispatcher("1", "2"), "121")
-        self.assertRaises(TypeError, dispatcher.__call__, "1", 1)
-        self.assertRaises(TypeError, dispatcher.__call__, 1, "1")
+        self.assertRaises(TypeError, dispatcher, "1", 1)
+        self.assertRaises(TypeError, dispatcher, 1, "1")
 
         dispatcher.register_rule(lambda x, y: str(x) + y + "1", int, str)
         self.assertEqual(dispatcher(1, 2), 4)
         self.assertEqual(dispatcher("1", "2"), "121")
         self.assertEqual(dispatcher(1, "2"), "121")
-        self.assertRaises(TypeError, dispatcher.__call__, "1", 1)
+        self.assertRaises(TypeError, dispatcher, "1", 1)
 
     def test_bottom_rule(self):
-        from generic.multidispatch import Dispatcher
-        dispatcher = Dispatcher(1)
+        dispatcher = self.createDispatcher(1, args=["x"])
 
         dispatcher.register_rule(lambda x: x, object)
         self.assertEqual(dispatcher(1), 1)
@@ -58,15 +63,14 @@ class DispatcherTests(unittest.TestCase):
         class Sub(Super):
             pass
 
-        from generic.multidispatch import Dispatcher
-        dispatcher = Dispatcher(1)
+        dispatcher = self.createDispatcher(1, args=["x"])
 
         dispatcher.register_rule(lambda x: x, Super)
         o_super = Super()
         self.assertEqual(dispatcher(o_super), o_super)
         o_sub = Sub()
         self.assertEqual(dispatcher(o_sub), o_sub)
-        self.assertRaises(TypeError, dispatcher.__call__, object())
+        self.assertRaises(TypeError, dispatcher, object())
 
         dispatcher.register_rule(lambda x: (x, x), Sub)
         o_super = Super()
@@ -74,19 +78,58 @@ class DispatcherTests(unittest.TestCase):
         o_sub = Sub()
         self.assertEqual(dispatcher(o_sub), (o_sub, o_sub))
 
-    def test_register_rule_with_different_arity(self):
-        from generic.multidispatch import Dispatcher
-        dispatcher = Dispatcher(1)
+    def test_register_rule_with_wrong_arity(self):
+        dispatcher = self.createDispatcher(1, args=["x"])
         dispatcher.register_rule(lambda x: x, int)
-        self.assertRaises(TypeError, dispatcher.register_rule, lambda x, y: x)
-
-    def test_register_rule_wit_kw_args(self):
-        # Keyword args do not supported right now.
-        from generic.multidispatch import Dispatcher
-        dispatcher = Dispatcher(1)
         self.assertRaises(
-            NotImplementedError,
-            dispatcher.register_rule, lambda x=1: x)
+            TypeError,
+            dispatcher.register_rule, lambda x, y: x, str)
+
+    def test_register_rule_with_different_arg_names(self):
+        dispatcher = self.createDispatcher(1, args=["x"])
+        dispatcher.register_rule(lambda y: y, int)
+        self.assertEqual(dispatcher(1), 1)
+
+    def test_dispatching_with_varargs(self):
+        dispatcher = self.createDispatcher(1, args=["x"], varargs="va")
+        dispatcher.register_rule(lambda x, *va: x, int)
+        self.assertEqual(dispatcher(1), 1)
+        self.assertRaises(TypeError, dispatcher, "1", 2, 3)
+
+    def test_dispatching_with_varkw(self):
+        dispatcher = self.createDispatcher(1, args=["x"], keywords="vk")
+        dispatcher.register_rule(lambda x, **vk: x, int)
+        self.assertEqual(dispatcher(1), 1)
+        self.assertRaises(TypeError, dispatcher, "1", a=1, b=2)
+
+    def test_dispatching_with_kw(self):
+        dispatcher = self.createDispatcher(1, args=["x", "y"], defaults=["vk"])
+        dispatcher.register_rule(lambda x, y=1: x, int)
+        self.assertEqual(dispatcher(1), 1)
+        self.assertRaises(TypeError, dispatcher, "1", k=1)
+
+    def test_create_dispatcher_with_pos_args_less_multi_arity(self):
+        self.assertRaises(TypeError, self.createDispatcher, 2, args=["x"])
+        self.assertRaises(TypeError, self.createDispatcher, 2, args=["x", "y"],
+                         defaults=["x"])
+
+    def test_register_rule_with_wrong_number_types_parameters(self):
+        dispatcher = self.createDispatcher(1, args=["x", "y"])
+        self.assertRaises(
+            TypeError,
+            dispatcher.register_rule, lambda x, y: x, int, str)
+
+    def test_register_rule_with_partial_dispatching(self):
+        dispatcher = self.createDispatcher(1, args=["x", "y"])
+        dispatcher.register_rule(lambda x, y: x, int)
+        self.assertEqual(dispatcher(1, 2), 1)
+        self.assertEqual(dispatcher(1, "2"), 1)
+        self.assertRaises(TypeError, dispatcher, "2", 1)
+        dispatcher.register_rule(lambda x, y: x, str)
+        self.assertEqual(dispatcher(1, 2), 1)
+        self.assertEqual(dispatcher(1, "2"), 1)
+        self.assertEqual(dispatcher("1", "2"), "1")
+        self.assertEqual(dispatcher("1", 2), "1")
 
 
 class TestMultimethod(unittest.TestCase):
