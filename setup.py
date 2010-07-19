cdae0d880595511c2a20581ebd6bbc254574a291
Andrey Popp <8mayday@gmail.com>
2010-07-19 16:29:11 +0400

    Bump version to 0.3.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = "0.2"
+version = "0.3"
 long_description = \
         open("README.rst", "r").read() + \
         "\n\n" + \
